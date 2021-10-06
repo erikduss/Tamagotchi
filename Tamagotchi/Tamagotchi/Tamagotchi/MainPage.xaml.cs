7da33c0c0eb48f7c1a2cfc66c7ebbf573986163e
Erikduss <erik.dusschooten@student.hku.nl>
2021-10-06 15:36:14 +0200

    Offline files added

---
+++ Tamagotchi/Tamagotchi/Tamagotchi/MainPage.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tamagotchi
+{
+    public partial class MainPage : ContentPage
+    {
+        ObservableCollection<PlayerAction> playeractions = new ObservableCollection<PlayerAction>();
+        public ObservableCollection<PlayerAction> PlayerActions { get { return playeractions; } }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            AddPlayerActions();
+        }
+
+        private void AddPlayerActions()
+        {
+            playeractions.Add(new PlayerAction { ImageName = "Shark_pup_food.png", LinkedAction = Actions.FEED, ActionInfo = "Feeding time!", ClickFunction = "NavigateToFeedPage"});
+            playeractions.Add(new PlayerAction { ImageName = "Shark_pup_drink.png", LinkedAction = Actions.DRINK, ActionInfo = "Do sharks really need to drink?", ClickFunction = "NavigateToDrinkPage" });
+            playeractions.Add(new PlayerAction { ImageName = "Shark_pup_attention.png", LinkedAction = Actions.ATTENTION, ActionInfo = "Give the little shark some attention!", ClickFunction = "NavigateToAttentionPage" });
+            playeractions.Add(new PlayerAction { ImageName = "Shark_pup_friends.png", LinkedAction = Actions.FRIENDS, ActionInfo = "Look for some friends for the little shark.", ClickFunction = "NavigateToFriendsPage" });
+            playeractions.Add(new PlayerAction { ImageName = "Shark_pup_alonetime.png", LinkedAction = Actions.ALONETIME, ActionInfo = "Leave the little shark alone for a while.", ClickFunction = "NavigateToAlonetimePage" });
+            playeractions.Add(new PlayerAction { ImageName = "Shark_pup_sleep.png", LinkedAction = Actions.SLEEP, ActionInfo = "Everyone needs a nap!", ClickFunction = "NavigateToSleepPage" });
+
+            carView_Actions.ItemsSource = PlayerActions;
+        }
+
+        private void btn_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                PlayerAction curAction = (PlayerAction)carView_Actions.CurrentItem;
+
+                switch (curAction.LinkedAction)
+                {
+                    case Actions.FEED:
+                            NavigateToFeedPage();
+                        break;
+                    case Actions.DRINK:
+                            NavigateToDrinkPage();
+                        break;
+                    case Actions.ATTENTION:
+                            NavigateToAttentionPage();
+                        break;
+                    case Actions.FRIENDS:
+                            NavigateToFriendsPage();
+                        break;
+                    case Actions.ALONETIME:
+                            NavigateToAlonetimePage();
+                        break;
+                    case Actions.SLEEP:
+                            NavigateToSleepPage();
+                        break;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Player action not defined");
+            }
+        }
+
+        private void NavigateToFeedPage()
+        {
+            Console.WriteLine("Navigate to feed");
+        }
+        private void NavigateToDrinkPage()
+        {
+            Console.WriteLine("Navigate to drink");
+        }
+        private void NavigateToAttentionPage()
+        {
+            Console.WriteLine("Navigate to attention");
+        }
+        private void NavigateToFriendsPage()
+        {
+            Console.WriteLine("Navigate to friends");
+        }
+        private void NavigateToAlonetimePage()
+        {
+            Console.WriteLine("Navigate to alone time");
+        }
+        private void NavigateToSleepPage()
+        {
+            Console.WriteLine("Navigate to sleep");
+        }
+    }
+}
---
+++ Tamagotchi/Tamagotchi/Tamagotchi/PlayerAction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tamagotchi
+{
+    public enum Actions
+    {
+        FEED,
+        DRINK,
+        ATTENTION,
+        FRIENDS,
+        ALONETIME,
+        SLEEP
+    }
+
+    public class PlayerAction
+    {
+        public string ImageName { get; set; }
+        public Actions LinkedAction { get; set; }
+
+        public string ActionInfo { get; set; }
+
+        public string ClickFunction { get; set; }
+    }
+}
